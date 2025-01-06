96b82dcc2346ddfdb6047ab71f2c64a1637f9697
neekesh <nikesthapa20@gmail.com>
2025-01-06 14:35:35 +0545

    fix: update classs

--- python/add_two_numbers.py
+++ python/add_two_numbers.py
@@ -5,6 +5,12 @@
 
 from typing import Optional
 
+
+class ListNode:
+    def __init__(self, val = 0, next = 0):
+        self.val = val
+        self.next = next
+
 class Solution:
     def addTwoNumbers(
         self, l1: Optional[ListNode], l2: Optional[ListNode]
