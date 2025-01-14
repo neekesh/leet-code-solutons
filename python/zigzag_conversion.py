c7fd3b53ef9f2b0fa6d72c8a86ebfbcd1e5a8ec8
neekesh <nikesthapa20@gmail.com>
2025-01-14 20:17:51 +0545

    feat: update one dimensional solution

--- python/zigzag_conversion.py
+++ python/zigzag_conversion.py
@@ -62,5 +62,19 @@ class TwoDimensionArray:
         return "".join(ans)
 
 
-convet = TwoDimensionArray()
-print(convet.convert("Paypayishiring", 3))
+
+
+class OneDimensionArray:
+    def convert(self, s: str, numRows: int) -> str:
+       
+        rows = [''] * numRows
+        k = 0
+        direction = (numRows == 1) - 1
+
+        for c in s:
+            rows[k] += c
+            if k == 0 or k == numRows - 1:
+                direction *= -1
+            k += direction
+        return "".join(rows)
+
